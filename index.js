40f3b4e0d9a76e35fcaa0b5e2dfeb7d0bdbe86c6
Seda Ünal <sedaylmz06@outlook.com>
2022-10-29 18:59:45 +0300

    fetch

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    renderBooks(json)
+  });
+
+  //return fetch(renderBooks())
   
 }
 
 function renderBooks(books) {
+ // console.log(books)
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
